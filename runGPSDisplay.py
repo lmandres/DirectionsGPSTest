23eef14ab70b78a006d36a16b73dbd0e3ce33e4f
lmandres <lmandres@yahoo.com>
2020-10-08 15:48:39 -0400

    Add helper tools.

--- runGPSDisplay.py
+++ runGPSDisplay.py
@@ -11,19 +11,36 @@ import gpsScanner
 
 if __name__ == '__main__':
 
-    def getMapImg(latitude, longitude):
+    def getMapImg(xtile, ytile, zoom):
 
         url = (
-            "http://10.0.0.245:8080/styles/klokantech-basic/static/" +
-            str(longitude) + "," + str(latitude) + ",15/" +
-            str(display_width) + "x" + str(display_height) +
-	    ".png"
+            "http://c.tile.openstreetmap.org/{}/{}/{}.png".format(
+                zoom, xtile, ytile
+            )
         )
         print(url)
         req = requests.get(url)
         mapImg = pygame.image.load(BytesIO(req.content))
 
-        return mapImg	
+        return mapImg
+
+    def deg2TileNum(lat_deg, lon_deg, zoom):
+
+        lat_rad = math.radians(lat_deg)
+        n = 2.0 ** zoom
+        xtile = int((lon_deg + 180.0) / 360.0 * n)
+        ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+        
+        return (xtile, ytile)
+
+    def tileNum2Deg(xtile, ytile, zoom):
+
+        n = 2.0 ** zoom
+        lon_deg = xtile / n * 360.0 - 180.0
+        lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+        lat_deg = math.degrees(lat_rad)
+
+        return (lat_deg, lon_deg)
 
 
     pygame.init()
@@ -86,11 +103,21 @@ if __name__ == '__main__':
                     else:
                         raise ValueError('Longitude direction is invalid.')
 
+                    zoom = 5
+                    xtile, ytile = deg2TileNum(latitude, longitude, zoom)
                     mapImg = getMapImg(
-                        latitude,
-                        longitude
+                        xtile, ytile, zoom
                     )
-                    pygame.draw.circle(mapImg, (0, 0, 255), (round(display_width/2), round(display_height/2)), 10, 2) 
+                    pygame.draw.circle(
+                        mapImg,
+                        (0, 0, 255),
+                        (
+                            round(display_width/2),
+                            round(display_height/2)
+                        ),
+                        10,
+                        2
+                    ) 
                     screen.blit(mapImg, (0, 0))
                 except Exception as ex:
                     screen.fill((0, 0, 0))
